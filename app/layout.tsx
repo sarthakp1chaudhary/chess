51089b316cda97e1cda86ce2af165130f346ab3d
sarthakp1chaudhary <sarthakp1chaudhar@gmail.com>
2023-07-22 05:37:00 +0530

    Git start

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Scorchify banare',
+  description: 'spotify ka clone h..',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ chess/try.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<limits.h>
+
+using namespace std;
+int main(){
+
+    cout<<"2";
+    return 0;
+}
